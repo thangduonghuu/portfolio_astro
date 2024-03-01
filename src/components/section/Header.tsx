755acdf6fe9943e8046a0ed6e58b27595ecfb4ec
Thang Duong <thangduong12322@gmail.com>
2024-03-01 16:02:51 +0700

    header responsive

--- src/components/section/Header.tsx
+++ src/components/section/Header.tsx
@@ -1,17 +1,49 @@
-import Button from '../core/Button';
-import Container from '../core/Container';
-import Icon from '../core/Icon';
-import Link from '../core/Link';
+import cn from 'classnames';
+import Button from '@/components/core/Button';
+import Container from '@/components/core/Container';
+import Icon from '@/components//core/Icon';
+import Link from '@/components/core/Link';
+
 import '@/assets/sass/components/main/header.scss';
+import { useState } from 'react';
 
 export default function Header() {
+  const [openMenu, setOpenMenu] = useState<boolean>(true);
+
   return (
     <header className="main-header">
       <Container className="main-header__container">
-        <Link>
-          <Icon name="logo" size="large" className="main-header__icon" />
-        </Link>
-        <nav className="main-header__navbar">
+        <div className="main-header__logo">
+          <Link>
+            <Icon name="logo" size="large" className="main-header__icon" />
+          </Link>
+          <Link
+            className={cn([
+              'close-icon',
+              { 'close-icon--show': openMenu },
+              { 'close-icon--hide': !openMenu },
+            ])}
+            onClick={() => {
+              setOpenMenu(!openMenu);
+            }}
+          >
+            <Icon name="menu" size="medium" />
+          </Link>
+          <Link
+            className={cn([
+              'close-icon',
+              { 'close-icon--show': !openMenu },
+              { 'close-icon--hide': openMenu },
+            ])}
+            onClick={() => {
+              setOpenMenu(!openMenu);
+            }}
+          >
+            <Icon name="close" size="medium" />
+          </Link>
+        </div>
+
+        <nav className={cn(['main-header__navbar', { 'main-header__navbar--mobile': !openMenu }])}>
           <ul className="navbar__list">
             <li className="navbar__item">About</li>
             <li className="navbar__item">Works</li>
@@ -19,8 +51,17 @@ export default function Header() {
             <li className="navbar__item">Contact</li>
           </ul>
         </nav>
-
-        <Button>Contact</Button>
+        <div
+          className={cn([
+            'main-header__button',
+            { 'main-header__button--show': !openMenu },
+            {
+              'main-header__button--hide': openMenu,
+            },
+          ])}
+        >
+          <Button>Contact</Button>
+        </div>
       </Container>
     </header>
   );
